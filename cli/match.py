8f03f3d98b6da67f0e8ff6142559d68b5d17c5b9
vishit18 <vishitjiwane18@gmail.com>
2025-07-25 20:55:03 +0100

    Update match.py

--- cli/match.py
+++ cli/match.py
@@ -8,6 +8,12 @@ from app.parser import extract_text
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+import os
+
+if os.environ.get("CI") == "true":
+    print("Skipping GUI test in CI (Tkinter needs display).")
+    exit(0)
+
 
 def pick_file(title="Select a file", filetypes=None):
     # Use hardcoded test path in CI
