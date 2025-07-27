03b9083f89a96108626e3eb6cc0d64ce92bda481
vishit18 <vishitjiwane18@gmail.com>
2025-07-27 17:58:07 +0100

    Update match.py

--- cli/match.py
+++ cli/match.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import glob
 import argparse
 from app.matcher import score_resumes, convert_to_10pt
